609d6e69b10598bf0d0d77f07220c56f095422fc
tamim-shadman <tamimshadman2000@gmail.com>
2024-05-20 23:31:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import joblib
 from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import CountVectorizer
+from flaskcors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Load the model
 model = joblib.load('NB_model.joblib')
 
