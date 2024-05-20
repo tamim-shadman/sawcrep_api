d8e3ec852f0a459b0d14854bb45fa1fa2848b1d0
tamim-shadman <tamimshadman2000@gmail.com>
2024-05-20 21:41:58 +0600

    deployment

---
+++ app.py
@@ -0,0 +1,31 @@
+import joblib
+from flask import Flask, request, jsonify
+from sklearn.feature_extraction.text import CountVectorizer
+
+app = Flask(__name__)
+
+# Load the model
+model = joblib.load('NB_model.joblib')
+
+# Load the CountVectorizer
+vectorizer = joblib.load('CountVectorizer_model.joblib')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get review text from request
+        review_text = request.json['review_text']
+
+        # Transform the review text using the loaded CountVectorizer
+        review_vector = vectorizer.transform([review_text])
+
+        # Make prediction
+        predicted_recommendation = model.predict(review_vector)
+
+        # Return prediction as JSON (assuming predicted_recommendation is an array)
+        return jsonify({'recommended_ind': int(predicted_recommendation[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
