b42cbe601851521417735fd52e90cd185b10c495
tamim-shadman <tamimshadman2000@gmail.com>
2024-05-20 23:41:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import joblib
 from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import CountVectorizer
-from flaskcors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
